cee7ef439526ed26432b6db754799056e702f55b
Oosasukel <rodrigo_gonn@hotmail.com>
2021-11-05 19:56:00 -0300

    listLength recursivo

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: listLength(list, 0)
+
+  defp listLength([], acc) do
+    acc
+  end
+
+  defp listLength([_ | tail], acc) do
+    acc = acc + 1
+    listLength(tail, acc)
   end
 end
